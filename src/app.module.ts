6056d48c824c1c0a285514d44ceea4a6c604b12a
jsanabrh <juanhoyos0422@gmail.com>
2024-06-20 19:12:19 -0500

    Connecting to the database is done on Vercel.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,32 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: +process.env.PORT,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DATABASE,
+      autoLoadEntities: true,
+      synchronize: true,
+      entities: [],
+      extra: {
+        ssl: true,
+      },
+    }),
+    TypeOrmModule.forFeature([]),
+  
+  ],
+
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
