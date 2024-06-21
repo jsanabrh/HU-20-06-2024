d15fef919aa8841a12cf2d9e8a3f5765a3a9711c
jsanabrh <juanhoyos0422@gmail.com>
2024-06-21 11:13:55 -0500

    Resolved merge conflict in app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SessionsModule } from './sessions/sessions.module';
+import { ReservationsModule } from './reservations/reservations.module';
+import { ReservationsService } from './reservations/services/reservations.service';
 
 @Module({
-  imports: [SessionsModule],
+  imports: [SessionsModule, ReservationsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ReservationsService],
 })
 export class AppModule {}
