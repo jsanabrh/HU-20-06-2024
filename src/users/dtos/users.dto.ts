aca8de2c3404a15357d781ae907fa49c077c24e5
jsanabrh <juanhoyos0422@gmail.com>
2024-06-21 10:19:29 -0500

    DTOs are created to validate the properties of the sessions entity.

---
+++ src/users/dtos/users.dto.ts
@@ -0,0 +1,31 @@
+import { IsString } from '@nestjs/class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+import { IsDate, IsNotEmpty, IsOptional } from 'class-validator';
+
+export class RoomsDto {
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  nameUser: string;
+
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  lastNameUser: string;
+
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  emailUser: string;
+
+  @IsOptional()
+  @Type(() => Date)
+  @IsDate()
+  createAt: Date;
+
+  @IsOptional()
+  @Type(() => Date)
+  @IsDate()
+  updateAt: Date;
+}
