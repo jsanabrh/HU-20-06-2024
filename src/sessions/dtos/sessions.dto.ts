24673bb1c33732fa4ea6249a0ae6dd1c7c7fa5d9
jsanabrh <juanhoyos0422@gmail.com>
2024-06-21 10:58:29 -0500

    Changes in the session DTO name

--- src/sessions/dtos/sessions.dto.ts
+++ src/sessions/dtos/sessions.dto.ts
@@ -3,7 +3,7 @@ import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import { IsDate, IsNotEmpty, IsOptional } from 'class-validator';
 
-export class RoomsDto {
+export class SessionsDto {
   @ApiProperty()
   @IsString()
   @IsNotEmpty()
