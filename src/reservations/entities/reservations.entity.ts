14aa75682a25ec156e1bf0c4398f78a249391d84
jsanabrh <juanhoyos0422@gmail.com>
2024-06-23 14:47:35 -0500

    Relationships are created between the entities of workspaces, sessions, and users with the entity of reservations

--- src/reservations/entities/reservations.entity.ts
+++ src/reservations/entities/reservations.entity.ts
@@ -1,8 +1,12 @@
 import { Reservation } from 'src/models/reservation.model';
+import { SessionsEntity } from 'src/sessions/entities/sessions.entity';
+import { UsersEntity } from 'src/users/entities/users.entity';
+import { WorkspacesEntity } from 'src/workspaces/entities/workspaces.entity';
 import {
   Column,
   CreateDateColumn,
   Entity,
+  ManyToOne,
   PrimaryGeneratedColumn,
 } from 'typeorm';
 
@@ -19,4 +23,13 @@ export class ReservationEntity {
 
   @CreateDateColumn()
   updateAt: Date;
+
+  @ManyToOne(() => UsersEntity, (user) => user.reservation)
+  user: UsersEntity;
+
+  @ManyToOne(() => WorkspacesEntity, (workspace) => workspace.reservation)
+  workspace: WorkspacesEntity;
+
+  @ManyToOne(() => SessionsEntity, (session) => session.reservation)
+  session: SessionsEntity;
 }
